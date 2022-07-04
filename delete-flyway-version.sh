35ca03ff306f3b6199a13a0f23284d51f13a73f3
robinhz <robin.heinz@posteo.de>
2022-07-04 20:19:34 +0200

    Improve flyway deletion echo

--- delete-flyway-version.sh
+++ delete-flyway-version.sh
@@ -10,7 +10,7 @@ then
     echo "Flyway Dir does not exist!" 
 else
     rm -rf ~/downloads/flyway-$flyway_version
-    echo "Flyway Dir deleted! (if no error came up)"
+    echo "Flyway Dir ~/downloads/flyway-${flyway_version} deleted!"
 fi
 
 # delete old ln in bin
